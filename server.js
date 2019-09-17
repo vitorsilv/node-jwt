b1b7cca672f459a5ef8a20c84069d231911e8ee2
Vitor Silva <vitor.procont@gmail.com>
2019-09-17 15:35:02 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const app = require("express")();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+// Conecta no MongoDB
+mongoose.connect(
+  "mongodb://linx-play:AW0lyOsJcsBSiMjb4VyTyZvg7C8K1XihfPK8VYvw33zXsDOUfQQGl40mZQlyBmxZYC6q4lz8X5BWSVWYf9DC2A%3D%3D@linx-play.documents.azure.com:10255/DEV-LINXPLAY?ssl=true"
+);
+
+// Carrega o model de Usuário
+require("./models/user");
+
+app.use(bodyParser.json());
+
+// Inicia as rotas da API
+app.use("/api", require("./controllers/userController"));
+
+app.listen(3000);
