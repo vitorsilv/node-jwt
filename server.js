98096e8003b879426b7687f27017d755224a93f2
Vitor Silva <vitor.procont@gmail.com>
2019-09-18 10:31:45 -0300

    Hotfix connection Mongo

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 // Conecta no MongoDB
-mongoose.connect(
-  "mongodb://linx-play:AW0lyOsJcsBSiMjb4VyTyZvg7C8K1XihfPK8VYvw33zXsDOUfQQGl40mZQlyBmxZYC6q4lz8X5BWSVWYf9DC2A%3D%3D@linx-play.documents.azure.com:10255/DEV-LINXPLAY?ssl=true"
-);
-
+mongoose.connect('mongodb://linx-play:AW0lyOsJcsBSiMjb4VyTyZvg7C8K1XihfPK8VYvw33zXsDOUfQQGl40mZQlyBmxZYC6q4lz8X5BWSVWYf9DC2A==@linx-play.documents.azure.com:10255/DEV-LINXPLAY?ssl=true&replicaSet=globaldb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
 // Carrega o model de Usuário
 require("./models/user");
 
